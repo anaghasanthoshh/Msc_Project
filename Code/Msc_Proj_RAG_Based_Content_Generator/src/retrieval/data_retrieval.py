3ee09e064263ca6fc28324c3d7136e480801e46e
Anagha Santhosh <141389881+anaghasanthoshh@users.noreply.github.com>
2025-03-10 21:46:45 +0000

    changes in embedding,retrieval files.

--- Code/Msc_Proj_RAG_Based_Content_Generator/src/retrieval/data_retrieval.py
+++ Code/Msc_Proj_RAG_Based_Content_Generator/src/retrieval/data_retrieval.py
@@ -4,13 +4,17 @@ from sentence_transformers import SentenceTransformer
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 # sample query
-query_text = "i want LASHVIEW brand eye lashes "
+query_text = "Find long-lasting gel nail polish."
 
 chroma_client = chromadb.PersistentClient(path="../../Data/chroma_db")
+#print("starting to fetch products")
 product_collection=chroma_client.get_collection("product_embeddings")
+#print("starting to fetch review")
 review_collection=chroma_client.get_collection("review_embeddings")
 
-# TODO:Use hybrid retrieval.Combine data from vector+postgres to refine retrival
+#Setting how many results need to be retrieved
+global k
+k=3
 
 # converting the query to embedding
 def query_to_vector(query):
@@ -21,18 +25,36 @@ def data_retrieval(query_embedding):
  # Retrieve from product collection
     product_results = product_collection.query(
     query_embeddings=[query_embedding.tolist()],
-    n_results=5
+    n_results=k
     )
 
 # Retrieve from review collection
     review_results = review_collection.query(
     query_embeddings=[query_embedding.tolist()],
-    n_results=5
+    n_results=k
     )
-    return product_results,review_results
+    product_ids=[item for sublist in product_results['ids'] for item in sublist]
+
+    prod_id_reviews=review_collection.get(
+        where={"parent_asin": {"$in": product_ids}}
+    )
+
+    return product_results,review_results,prod_id_reviews
 
 
+if __name__=="__main__":
+    query_embed=query_to_vector(query_text)
+    product_results, review_results, prod_id_reviews=data_retrieval(query_embed)
+    print(f"The product results are: \n{product_results}\n")
+    print(f"The review results are: \n{review_results}\n")
+    print(f"The reviews for fetched product results are: \n{prod_id_reviews}\n")
 
-query_embed=query_to_vector(query_text)
-print(data_retrieval(query_embed))
-
+##TODO: Remove multiple entries present at the moment in review collection
+##Cause : Added new column to collection-parent_asin for reviews and they are entered
+## as new data.
+##Fix: delete and create again.
+'''
+Product ids='B00UJOX1Z8', 'B07GV9VGLD', 'B00AGBQK4S'
+review fetched=B07GV9VGLD 
+We can see that there are some overlap between product and reviews fetched.
+'''
--- Code/Msc_Proj_RAG_Based_Content_Generator/src/retrieval/retrival_reranking.py
+++ Code/Msc_Proj_RAG_Based_Content_Generator/src/retrieval/retrival_reranking.py
@@ -1,7 +1,18 @@
-from retrieval.data_retrieval import query_to_vector,data_retrieval
+from retrieval.data_retrieval import query_to_vector,data_retrieval,k
+
+query_text="Find long-lasting gel nail polish."
+query_embed=query_to_vector(query_text)
+product_result,review_result,prod_id_reviews=data_retrieval(query_embed)
+#for i in range(k):product_result['ids']}
+print(review_result)#[0][i]['combined_text']
+##TODO:The 'Add of existing embedding ID' is carried forward.Check.
+
+def sentiment_analysis(prod_id_reviews) :
 
 
 
-query_embedding=query_to_vector(query_text)
-presult,rresult=data_retrieval(query_embedding)
-print(presult,rresult)
+
+#fetch_prod_related_reviews(product_result)
+
+
+
